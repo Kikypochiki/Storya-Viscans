d3c338afd3a5b79380d5bd136f5efe2da7b18d7a
Dohn Michael Varquez <23-1-02140@vsu.edu.ph>
2026-02-26 12:49:08 +0800

    feat: integrate Supabase for authentication and session management, add middleware, and define Profiles type

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,8 +1,8 @@
-import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs"
+import { createBrowserClient } from '@supabase/auth-helpers-nextjs'
 
 export const createClient = () => {
-    return createPagesBrowserClient({
-        supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-    })
+    return createBrowserClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    )
 }
