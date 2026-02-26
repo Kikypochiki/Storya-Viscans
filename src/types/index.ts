f830bc71dcd01227e49ff2fbda073f8e91ce4043
Dohn Michael Varquez <23-1-02140@vsu.edu.ph>
2026-02-26 15:04:25 +0800

    feat: implement CreateProfileDialog component for user profile creation

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,6 +4,7 @@ export type Profiles = {
     bio: string | null;
     avatar_url: string | null;
     created_at: string;
+    user_id: string;
 }
 export type Threads = {
     id: string;
@@ -16,4 +17,10 @@ export type Threads = {
     upvote_count: number;
     downvote_count: number;
     created_at: string;                                 
+}
+export type Categories = {
+    id: string;
+    name: string;
+    description: string | null;
+    created_at: string;
 }
